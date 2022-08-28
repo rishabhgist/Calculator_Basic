e30fa09138e7190186005beb532e2d1f3743c3c3
Rishabh <rishabhgupta7199@gmail.com>
2022-08-28 23:45:53 +0530

    added Calculator class object

--- src/main/java/com/calculator/Calculator.java
+++ src/main/java/com/calculator/Calculator.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        Calculator calculator = new Calculator();
 
     }
 
