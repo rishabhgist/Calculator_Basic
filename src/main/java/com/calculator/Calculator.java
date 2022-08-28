dcc70e79c01f22e86fafb6c1956c31865771c0ed
Rishabh <rishabhgupta7199@gmail.com>
2022-08-28 23:42:55 +0530

    added multiplyNumber method

--- src/main/java/com/calculator/Calculator.java
+++ src/main/java/com/calculator/Calculator.java
@@ -13,4 +13,8 @@ public class Calculator {
     public double divideNumber(double num1, double num2) {
         return num1 / num2;
     }
+
+    public double multiplyNumber(double num1, double num2) {
+        return num1 * num2;
+    }
 }
